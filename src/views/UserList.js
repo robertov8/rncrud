2d821bc126af6440286933900b3e69a7c6a86c7c
Roberto Ribeiro <robertov82008@gmail.com>
2020-09-28 23:43:23 -0300

    4. Navegação Stack #02

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {Text} from 'react-native';
 
-export default props => {
+export default (props) => {
   return <Text>UserList</Text>;
 };
