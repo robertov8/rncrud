aca3075e28b4fc27cc830023632fffa9f6775387
Roberto Ribeiro <robertov82008@gmail.com>
2020-10-01 16:10:53 -0300

    8. Rotas & Parâmetros

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
 
-export default (props) => {
-  return <Text>UserForm</Text>;
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+
+  console.warn(user);
+
+  return <Text>{user.id}</Text>;
 };
